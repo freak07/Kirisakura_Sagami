c60de141e4a23d5afdcaf03da305331ea04bf2f7
freak07 <michalechner92@googlemail.com>
2021-10-09 16:34:17 +0200

    treewide: msm: reduce auto suspend delays similar to commit: https://github.com/freak07/Kirisakura_SAKE_Zenfone_8/commit/eb1fdd1afcc1b22974069c22a04fbef53222c1bd
    (cherry picked from commit fd68c9c3ec76a70e05c1b6ea411cb9756f8cde38)
    (cherry picked from commit cabf80e263a5052a3c55327c114a79b8d7b2eb78)

--- techpack/audio/soc/swr-mstr-ctrl.c
+++ techpack/audio/soc/swr-mstr-ctrl.c
@@ -86,7 +86,7 @@
 #define SWRM_PHY_ADDR_MAP_COUNT 2
 
 /* pm runtime auto suspend timer in msecs */
-static int auto_suspend_timer = 500;
+static int auto_suspend_timer = 200;
 module_param(auto_suspend_timer, int, 0664);
 MODULE_PARM_DESC(auto_suspend_timer, "timer for auto suspend");
 
