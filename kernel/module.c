b69b09af20474d4c4f7261fa2b9d318fa7b07adf
freak07 <michalechner92@googlemail.com>
2021-12-06 22:00:45 +0100

    modules: force load modules regardless of checks

--- kernel/module.c
+++ kernel/module.c
@@ -1340,7 +1340,7 @@ static int check_version(const struct load_info *info,
 bad_version:
 	pr_warn("%s: disagrees about version of symbol %s\n",
 	       info->name, symname);
-	return 0;
+	return 1;
 }
 
 static inline int check_modstruct_version(const struct load_info *info,
@@ -2905,6 +2905,9 @@ static int module_sig_check(struct load_info *info, int flags)
 	const char *reason;
 	const void *mod = info->hdr;
 
+#if 1
+	return 0;
+#endif
 	/*
 	 * Require flags == 0, as a module with version information
 	 * removed is no longer the module that was signed
@@ -3860,7 +3863,10 @@ static int load_module(struct load_info *info, const char __user *uargs,
 		err = -EPERM;
 		goto free_copy;
 	}
-
+#if 1
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
+#endif
 	err = module_sig_check(info, flags);
 	if (err)
 		goto free_copy;
